c5874cfe5e1d0cc2c21aff7c29e4314add60de4f
Sammy <samuel.maina1@student.moringaschool>
2023-07-06 13:53:07 +0300

    Added db.json

---
+++ Documents/Flataculties/README.md
@@ -0,0 +1,34 @@
+# Flataculties
+
+Flataculties is an app where you can vote for the cutest animals! This project demonstrates the frontend implementation of Flataculties using a local API server to fetch animal data and display it on the user interface.
+
+## How to Run the Project
+
+### Prerequisites
+Node.js and npm (Node Package Manager) should be installed on your machine.
+
+### Setup
+Clone the repository:
+
+git clone https://github.com/SirCartermm/Flataculties-.git
+
+Navigate to the project directory:
+
+cd Flataculties-
+
+Install the dependencies:
+
+npm install
+
+### Start the Application
+Start the local API server:
+
+json-server --watch db.json
+
+Open the index.html file in a web browser.
+
+### Usage
+
+- Click on the "Show Animal Names" button to toggle the display of animal names. When clicked, the names of the animals will appear in a list.
+- Click on an animal name to view its details, including the animal's image and the number of votes it has received.
+- To vote for an animal, click the "Vote" button in the animal's details section. The vote count will be incremented on the UI, but no persistence is implemented.
--- Documents/Flataculties/app.js
+++ Documents/Flataculties/app.js
@@ -1,80 +1,51 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Fetch animal names from the API
-    axios.get('http://localhost:3000/characters')
-      .then(response => {
-        const animalList = response.data;
-        updateAnimalList(animalList);
-      })
-      .catch(error => {
-        console.error('Error fetching animal names:', error);
+function displayAnimalDetails(animal) {
+    const nameElement = document.getElementById('animal-name');
+    const imageElement = document.getElementById('animal-image');
+    const votesElement = document.getElementById('animal-votes');
+    const voteButton = document.getElementById('vote-button');
+
+    nameElement.textContent = animal.name;
+    imageElement.src = animal.image;
+    votesElement.textContent = `Votes: ${animal.votes}`;
+
+    // Event listener for voting button
+    voteButton.addEventListener('click', function() {
+        animal.votes++;
+        votesElement.textContent = `Votes: ${animal.votes}`;
       });
   
-    // Show animal names when the button is clicked
-    const showNamesButton = document.getElementById('showNamesButton');
-    showNamesButton.addEventListener('click', toggleAnimalListVisibility);
+    // Show the details section
+    const detailsSection = document.querySelector('.animal-details');
+    detailsSection.style.display = 'block';
+  }
+
   
-    function toggleAnimalListVisibility() {
-      const animalList = document.getElementById('animalList');
-      const isHidden = animalList.style.display === 'none';
-  
-      if (isHidden) {
-        showNamesButton.textContent = 'Hide Animal Names';
-        animalList.style.display = 'block';
-      } else {
-        showNamesButton.textContent = 'Show Animal Names';
-        animalList.style.display = 'none';
-      }
-    }
-  
-    function updateAnimalList(animalList) {
-      const animalListContainer = document.getElementById('animalList');
-  
-      animalList.forEach(animal => {
-        const listItem = document.createElement('li');
-        listItem.textContent = animal.name;
-        listItem.addEventListener('click', () => {
-          displayAnimalDetails(animal.id);
-        });
-        animalListContainer.appendChild(listItem);
+
+    
+
+  // Function to display animal list
+  function displayAnimalList(characters) {
+    const animalListElement = document.querySelector('.animal-list');
+
+    characters.forEach(function(animal) {
+      const listItem = document.createElement('li');
+      listItem.textContent = animal.name;
+
+      // Event listener for clicking on an animal name
+      listItem.addEventListener('click', function() {
+        displayAnimalDetails(animal);
       });
-    }
+
+      animalListElement.appendChild(listItem);
+    });
+  }
   
-    function displayAnimalDetails(animalId) {
-      axios.get(`http://localhost:3000/characters/${animalId}`)
-        .then(response => {
-          const animalDetails = response.data;
-          updateAnimalDetails(animalDetails);
-        })
-        .catch(error => {
-          console.error('Error fetching animal details:', error);
-        });
-    }
-  
-    function updateAnimalDetails(animalDetails) {
-      const animalDetailsContainer = document.getElementById('animalDetails');
-      animalDetailsContainer.innerHTML = '';
-  
-      const animalImage = document.createElement('img');
-      animalImage.src = animalDetails.image;
-      animalImage.alt = animalDetails.name;
-  
-      const voteButton = document.createElement('button');
-      voteButton.textContent = 'Vote';
-      voteButton.addEventListener('click', () => {
-        incrementVoteCount(animalDetails);
-      });
-  
-      const voteCount = document.createElement('p');
-      voteCount.textContent = `Votes: ${animalDetails.votes}`;
-  
-      animalDetailsContainer.appendChild(animalImage);
-      animalDetailsContainer.appendChild(voteButton);
-      animalDetailsContainer.appendChild(voteCount);
-    }
-  
-    function incrementVoteCount(animalDetails) {
-      animalDetails.votes++;
-      updateAnimalDetails(animalDetails);
-    }
-  });
-  
+
+  // Fetch data from the endpoint
+  fetch('db.json')
+    .then(response => response.json())
+    .then(data => {
+      const characters = data.characters;
+      displayAnimalList(characters);
+    })
+    .catch(error => console.error('Error:', error));
